3f0c5cb509d302c6367cd6feaae60bd962c25b35
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-09 23:39:56 -0400

    Added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import ApplyJob from './pages/ApplyJob.jsx'
+import Applications from './pages/Applications.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+      </Routes>
+    </div>
   )
 }
 
