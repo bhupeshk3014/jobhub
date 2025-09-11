850251743212f1c21ad9d5b81f407d7b4614cce9
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-11 17:52:17 -0400

    Made Fitlers Responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
               ?<div className='flex gap-3 items-center'>
                 <Link to="/applications">Applied Jobs</Link>
                 <p>|</p>
-                <p>Hi, {user.firstName+" "+user.lastName}</p>
+                <p className='max-sm:hidden'>Hi, {user.firstName+" "+user.lastName}</p>
                 <UserButton />
               </div>
               : <div className='flex gap-4 max-sm:text-sm'>
