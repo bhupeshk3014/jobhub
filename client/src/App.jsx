25476be87ac59fc3471e2d19eb3e346ebb72d155
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-16 15:06:46 -0400

    Added Dashboard - Add Job Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import ApplyJob from './pages/ApplyJob.jsx'
 import Applications from './pages/Applications.jsx'
 import RecruiterLogin from './components/RecruiterLogin.jsx'
 import { AppContext } from './context/AppContext.jsx';
+import Dashboard from './pages/Dashboard.jsx'
+import ManageJobs from './pages/ManageJobs.jsx'
+import AddJob from './pages/AddJob.jsx'
+import ViewApplications from './pages/ViewApplications.jsx'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
 
@@ -17,6 +22,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   )
