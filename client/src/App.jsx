b838c1591431f818eed587e3b074c787b0141962
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-16 03:12:45 -0400

    Added Recruiter Login Popup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import ApplyJob from './pages/ApplyJob.jsx'
 import Applications from './pages/Applications.jsx'
+import RecruiterLogin from './components/RecruiterLogin.jsx'
+import { AppContext } from './context/AppContext.jsx';
 
 const App = () => {
+
+  const { showRecruiterLogin } = useContext(AppContext);
+
   return (
     <div>
+      {showRecruiterLogin && <RecruiterLogin />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
