e7e8e1b1f52cc1f99ba07f384946a6d3807f2cde
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-18 16:36:50 -0400

    Implement post job functionality

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -10,31 +10,32 @@ import {
   registerCompany,
 } from "../controllers/companyController.js";
 import upload from "../config/multer.js";
+import { protectCompany } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Register a company
-router.post("/register", upload.single('image'), registerCompany);
+router.post("/register", upload.single("image"), registerCompany);
 
 // Company login
 router.post("/login", loginCompany);
 
 // Get company data
-router.get("/company", getCompanyData);
+router.get("/company", protectCompany, getCompanyData);
 
 // Post a job
-router.post("/post-job", postJob);
+router.post("/post-job", protectCompany, postJob);
 
 // Get Applicants Data of Company
-router.get("/applicants", getCompanyJobApplicants);
+router.get("/applicants", protectCompany, getCompanyJobApplicants);
 
 // Get Company Job list
-router.get("/list-jobs", getCompanyPostedJobs);
+router.get("/list-jobs", protectCompany, getCompanyPostedJobs);
 
 // Change Applications Status
-router.post("/change-status", changeJobApplicationsStatus);
+router.post("/change-status", protectCompany, changeJobApplicationsStatus);
 
 // Change Applications Visibility
-router.post("/change-visibility", changeVisibility);
+router.post("/change-visibility", protectCompany, changeVisibility);
 
-export default router
+export default router;
