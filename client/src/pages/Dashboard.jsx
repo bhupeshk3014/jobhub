76d5d8dc4fd3d0612210454aec3e8719ef88d9eb
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2025-09-19 18:58:12 -0400

    Add logout functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,7 +6,16 @@ import { AppContext } from "../context/AppContext";
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  const { companyData } = useContext(AppContext);
+  const { companyData, setCompanyData, setCompanyToken } =
+    useContext(AppContext);
+
+  // Function to logout for company
+  const logout = () => {
+    setCompanyToken(null);
+    localStorage.removeItem("companyToken");
+    setCompanyData(null);
+    navigate("/");
+  };
 
   return (
     <div className="min-h-screen">
@@ -30,7 +39,7 @@ const Dashboard = () => {
                 />
                 <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
                   <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
-                    <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
+                    <li onClick={logout} className="py-1 px-2 cursor-pointer pr-10">Logout</li>
                   </ul>
                 </div>
               </div>
